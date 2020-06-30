f224517c103342d4ad5bf2dadeca7cf676053734
kamau-mbugua <kamau_mbugua@yahoo.com>
2020-06-30 16:12:46 +0300

    Initialised the project with Global Stats Activity working

---
+++ app/src/main/java/kamau_technerd/com/covid19/AffectedCountries.java
@@ -0,0 +1,14 @@
+package kamau_technerd.com.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AffectedCountries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affected_countries);
+    }
+}
---
+++ app/src/main/java/kamau_technerd/com/covid19/MainActivity.java
@@ -0,0 +1,112 @@
+package kamau_technerd.com.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.leo.simplearcloader.SimpleArcLoader;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvCases, tvRecovered, tvCritical, tvActive,
+            tvTodayCases, tvTodayDeaths, tvTotalDeaths,tvAffectedCountries;
+    ScrollView scrollView;
+    SimpleArcLoader simpleArcLoader;
+    PieChart pieChat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvCases = findViewById(R.id.tvcases);
+        tvRecovered = findViewById(R.id.tvRecovered);
+        tvCritical = findViewById(R.id.tvCritical);
+        tvActive = findViewById(R.id.tvActive);
+        tvTodayCases = findViewById(R.id.tvTodayCases);
+        tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
+        tvTotalDeaths = findViewById(R.id.tvTotalDeaths);
+        tvAffectedCountries = findViewById(R.id.tvAffectedCountries);
+
+        scrollView = findViewById(R.id.scrollStats);
+        simpleArcLoader = findViewById(R.id.loader);
+        pieChat = findViewById(R.id.piechart);
+        
+        fetchData();
+
+
+    }
+
+    private void fetchData() {
+         String url = "https://corona.lmao.ninja/v2/all";
+         simpleArcLoader.start();
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response.toString());
+
+                    tvCases.setText(jsonObject.getString("cases"));
+                    tvRecovered.setText(jsonObject.getString("recovered"));
+                    tvCritical.setText(jsonObject.getString("critical"));
+                    tvActive.setText(jsonObject.getString("active"));
+                    tvTodayCases.setText(jsonObject.getString("todayCases"));
+                    tvTodayDeaths.setText(jsonObject.getString("todayDeaths"));
+                    tvTotalDeaths.setText(jsonObject.getString("deaths"));
+                    tvAffectedCountries.setText(jsonObject.getString("affectedCountries"));
+
+                    pieChat.addPieSlice(new PieModel("Cases",Integer.parseInt(tvCases.getText().toString()), Color.parseColor("#FFA726")));
+                    pieChat.addPieSlice(new PieModel("Recovered",Integer.parseInt(tvRecovered.getText().toString()), Color.parseColor("#66BB6A")));
+                    pieChat.addPieSlice(new PieModel("Deaths",Integer.parseInt(tvTotalDeaths.getText().toString()), Color.parseColor("#EF5350")));
+                    pieChat.addPieSlice(new PieModel("Active",Integer.parseInt(tvActive.getText().toString()), Color.parseColor("#2986F6")));
+
+                    simpleArcLoader.stop();
+                    simpleArcLoader.setVisibility(View.GONE);
+                    scrollView.setVisibility(View.VISIBLE);
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    simpleArcLoader.stop();
+                    simpleArcLoader.setVisibility(View.GONE);
+                    scrollView.setVisibility(View.VISIBLE);
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                simpleArcLoader.stop();
+                simpleArcLoader.setVisibility(View.GONE);
+                scrollView.setVisibility(View.VISIBLE);
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+
+    }
+
+    public void goTrackCountries(View view) {
+    }
+}
