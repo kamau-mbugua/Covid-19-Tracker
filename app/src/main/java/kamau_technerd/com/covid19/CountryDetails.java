fe203f235935a53a42468b45f519b0ae65ace00f
kamau-mbugua <kamau_mbugua@yahoo.com>
2020-07-01 14:37:47 +0300

    Implemented The search bar filtering

---
+++ app/src/main/java/kamau_technerd/com/covid19/CountryDetails.java
@@ -0,0 +1,14 @@
+package kamau_technerd.com.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CountryDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_details);
+    }
+}
--- app/src/main/java/kamau_technerd/com/covid19/MyCustomAdapter.java
+++ app/src/main/java/kamau_technerd/com/covid19/MyCustomAdapter.java
@@ -1,13 +1,43 @@
 package kamau_technerd.com.covid19;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+import java.util.zip.Inflater;
 
 public class MyCustomAdapter extends ArrayAdapter<CountryModel> {
 
-    public MyCustomAdapter(@NonNull Context context, int resource) {
-        super(context, resource);
+    private Context context;
+    private List<CountryModel> countryModelList;
+
+    public MyCustomAdapter(@NonNull Context context, List<CountryModel> countryModelList) {
+        super(context, R.layout.row_layout);
+
+        this.context = context;
+        this.countryModelList = countryModelList;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout,null,true);
+        TextView tvCountryName = view.findViewById(R.id.tvCountryName);
+        ImageView imageView =view.findViewById(R.id.ivflag);
+        tvCountryName.setText(countryModelList.get(position).getCountry());
+        Glide.with(context).load(countryModelList.get(position).getFlag()).into(imageView);
+
+        return view;
     }
 }
