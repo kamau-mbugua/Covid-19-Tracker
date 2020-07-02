c8f3b43f9d73b38b4b5393608a2f6446ffb2482a
kamau-mbugua <kamau_mbugua@yahoo.com>
2020-07-02 14:03:13 +0300

    All funtions worrking together to deliver apk

--- app/src/main/java/kamau_technerd/com/covid19/AffectedCountries.java
+++ app/src/main/java/kamau_technerd/com/covid19/AffectedCountries.java
@@ -1,10 +1,16 @@
 package kamau_technerd.com.covid19;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -31,7 +37,7 @@ public class AffectedCountries extends AppCompatActivity {
     ListView listView;
     SimpleArcLoader simpleArcLoader;
 
-    public  static List<CountryModel> countryModelList = new ArrayList<>();
+    public static List<CountryModel> countryModelsList = new ArrayList<>();
     CountryModel countryModel;
     MyCustomAdapter myCustomAdapter;
 
@@ -39,61 +45,114 @@ public class AffectedCountries extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affected_countries);
-        edtSearch= findViewById(R.id.edtSearch);
+
+        edtSearch = findViewById(R.id.edtSearch);
         listView = findViewById(R.id.listView);
         simpleArcLoader = findViewById(R.id.loader);
 
+        getSupportActionBar().setTitle("Affected Countries");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         fetchData();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(getApplicationContext(),CountryDetails.class).putExtra("position",position));
+            }
+        });
+
+
+       edtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                myCustomAdapter.getFilter().filter(s);
+                myCustomAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
     }
 
     private void fetchData() {
-        String url = "https://corona.lmao.ninja/v2/countries/";
+
+        String url  = "https://corona.lmao.ninja/v2/countries/";
+
         simpleArcLoader.start();
 
-        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONArray jsonArray =new JSONArray(response);
-                    for (int i =0; i<jsonArray.length();i++){
-                        JSONObject jsonObject= jsonArray.getJSONObject(i);
-                        String countryName = jsonObject.getString("country");
-                        String cases = jsonObject.getString("cases");
-                        String todayCases = jsonObject.getString("todayCases");
-                        String deaths = jsonObject.getString("deaths");
-                        String todayDeaths = jsonObject.getString("todayDeaths");
-                        String recovered = jsonObject.getString("recovered");
-                        String active = jsonObject.getString("active");
-                        String critical = jsonObject.getString("critical");
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
 
-                        JSONObject object = jsonObject.getJSONObject("countryInfo");
-                        String flagUrl = object.getString("flag");
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
 
-                        countryModel = new CountryModel(flagUrl,countryName,cases,todayCases,deaths,todayDeaths,recovered,active,critical);
-                        countryModelList.add(countryModel);
+                            for(int i=0;i<jsonArray.length();i++){
+
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                                String countryName = jsonObject.getString("country");
+                                String cases = jsonObject.getString("cases");
+                                String todayCases = jsonObject.getString("todayCases");
+                                String deaths = jsonObject.getString("deaths");
+                                String todayDeaths = jsonObject.getString("todayDeaths");
+                                String recovered = jsonObject.getString("recovered");
+                                String active = jsonObject.getString("active");
+                                String critical = jsonObject.getString("critical");
+
+                                JSONObject object = jsonObject.getJSONObject("countryInfo");
+                                String flagUrl = object.getString("flag");
+
+                                countryModel = new CountryModel(flagUrl,countryName,cases,todayCases,deaths,todayDeaths,recovered,active,critical);
+                                countryModelsList.add(countryModel);
+
+
+                            }
+
+                            myCustomAdapter = new MyCustomAdapter(AffectedCountries.this,countryModelsList);
+                            listView.setAdapter(myCustomAdapter);
+                            simpleArcLoader.stop();
+                            simpleArcLoader.setVisibility(View.GONE);
+
+
+
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            simpleArcLoader.stop();
+                            simpleArcLoader.setVisibility(View.GONE);
+                        }
 
 
                     }
-
-                    myCustomAdapter = new MyCustomAdapter(AffectedCountries.this,countryModelList);
-                   listView.setAdapter(myCustomAdapter);
-                    simpleArcLoader.stop();
-                    simpleArcLoader.setVisibility(View.GONE);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    simpleArcLoader.stop();
-                    simpleArcLoader.setVisibility(View.GONE);
-                }
-
-
-            }
-        }, new Response.ErrorListener() {
+                }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 simpleArcLoader.stop();
                 simpleArcLoader.setVisibility(View.GONE);
-
                 Toast.makeText(AffectedCountries.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
@@ -101,5 +160,7 @@ public class AffectedCountries extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(request);
 
+
     }
+
 }
--- app/src/main/java/kamau_technerd/com/covid19/MyCustomAdapter.java
+++ app/src/main/java/kamau_technerd/com/covid19/MyCustomAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +14,7 @@ import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.Inflater;
 
@@ -20,12 +22,14 @@ public class MyCustomAdapter extends ArrayAdapter<CountryModel> {
 
     private Context context;
     private List<CountryModel> countryModelList;
+    private List<CountryModel> countryModelsListFiltered;
 
-    public MyCustomAdapter(@NonNull Context context, List<CountryModel> countryModelList) {
-        super(context, R.layout.row_layout);
+    public MyCustomAdapter(Context context, List<CountryModel> countryModelList) {
+        super(context, R.layout.row_layout,countryModelList);
 
         this.context = context;
         this.countryModelList = countryModelList;
+        this.countryModelsListFiltered = countryModelList;
 
     }
 
@@ -35,9 +39,62 @@ public class MyCustomAdapter extends ArrayAdapter<CountryModel> {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout,null,true);
         TextView tvCountryName = view.findViewById(R.id.tvCountryName);
         ImageView imageView =view.findViewById(R.id.ivflag);
-        tvCountryName.setText(countryModelList.get(position).getCountry());
-        Glide.with(context).load(countryModelList.get(position).getFlag()).into(imageView);
+        tvCountryName.setText(countryModelsListFiltered.get(position).getCountry());
+        Glide.with(context).load(countryModelsListFiltered.get(position).getFlag()).into(imageView);
 
         return view;
+    }
+
+    @Override
+    public int getCount() {
+        return countryModelsListFiltered.size();
+    }
+
+    @Nullable
+    @Override
+    public CountryModel getItem(int position) {
+        return countryModelsListFiltered.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                FilterResults filterResults = new FilterResults();
+                if (charSequence == null || charSequence.length() ==0){
+                    filterResults.count = countryModelList.size();
+                    filterResults.values = countryModelList;
+                } else {
+                    List<CountryModel> resultsModel = new ArrayList<>();
+                    String searchstr =  charSequence.toString().toLowerCase();
+
+                    for (CountryModel itemModel :countryModelList){
+                        if (itemModel.getCountry().toLowerCase().contains(searchstr)){
+                            resultsModel.add(itemModel);
+                        }
+                        filterResults.count = resultsModel.size();
+                        filterResults.values = resultsModel;
+
+                    }
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+
+                countryModelsListFiltered =(List<CountryModel>) filterResults .values;
+                AffectedCountries.countryModelsList = (List<CountryModel>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+        return filter;
     }
 }
